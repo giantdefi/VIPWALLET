d8a03d59932538d544edfa75e5ba4833edbe0f96
unknown <cryptonativecoding@gmail.com>
2024-06-14 09:04:18 +0700

    commit change css

--- src/pages/users/network.js
+++ src/pages/users/network.js
@@ -33,7 +33,7 @@ export default function Users() {
                 <meta name="description" content="Users Page" />
             </Head>
 
-            <div className="min-h-screen pb-40 mx-3">
+            <div className="min-h-screen pb-40 mx-3 ">
 
                 
 
@@ -59,7 +59,7 @@ export default function Users() {
                                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                                 </svg>
                                 : <>
-                                    <i className="icofont-hand-drag2 text-yellow-300 text-4xl "></i>
+                                    <i className="icofont-business-man text-yellow-300 text-4xl "></i>
                                     <p className="text-[11px] mt-1">VIP1000000001</p>
                                 </>
                             }
@@ -68,12 +68,16 @@ export default function Users() {
 
                 
                 </div>
+                <div className="text-center py-2 mt-2 bg-gray-700">
+                    <p className="uppercase ">START UP</p>
+                </div>
 
+  
                 <div className="w-full flex centered mt-2">
 
                   
 
-<button onClick={() => handleMovePage('users/matching-bonus')} className="btn_menu_user rounded-lg">
+<button onClick={() => handleMovePage('users/matching-bonus')} className="btn_menu_user rounded-lg ">
     <div className=" flex flex-col justify-center items-center">
         {menuSpinner && itemLink == "users/matching-bonus" ?
             <svg style={{ maxWidth: 40 }} role="status" className="inline w-9 h-9 text-gray-200 animate-spin dark:text-gray-600 fill-red-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -81,7 +85,7 @@ export default function Users() {
                 <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
             </svg>
             : <>
-                <i className="icofont-rocket-alt-1 text-yellow-300 text-4xl "></i>
+                <i className="icofont-bank-alt text-yellow-300 text-4xl "></i>
                 <p className="text-[11px] mt-1">VIP1000000002</p>
             </>
         }
@@ -97,7 +101,7 @@ export default function Users() {
 
 
                 <div className="w-full flex-between justify-center items-center gap-3 mt-2">
-                    <button onClick={() => handleMovePage('users/confirmation')} className="btn_menu_user">
+                    <button onClick={() => handleMovePage('users/confirmation')} className="btn_menu_user w-1/3 h-[90px]">
                         <div className=" flex flex-col justify-center items-center">
                             {menuSpinner && itemLink == "users/confirmation" ?
                                 <svg style={{ maxWidth: 40 }} role="status" className="inline w-9 h-9 text-gray-200 animate-spin dark:text-gray-600 fill-red-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -105,7 +109,7 @@ export default function Users() {
                                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                                 </svg>
                                 : <>
-                                    <i className="icofont-rocket-alt-1 text-4xl  text-yellow-400"></i>
+                                    <i className="icofont-wallet text-4xl  text-yellow-400"></i>
                                     <p className="text-xs mt-2">VIP1000000003</p>
                                 </>
                             }
@@ -115,7 +119,7 @@ export default function Users() {
                  
 
 
-                    <button onClick={() => handleMovePage('users/send-e-wallet')} className="btn_menu_user">
+                    <button onClick={() => handleMovePage('users/send-e-wallet')} className="btn_menu_user w-1/4 h-[90px]">
                         <div className=" flex flex-col justify-center items-center">
                             {menuSpinner && itemLink == "users/send-e-wallet" ?
                                 <svg style={{ maxWidth: 40 }} role="status" className="inline w-9 h-9 text-gray-200 animate-spin dark:text-gray-600 fill-red-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -123,7 +127,7 @@ export default function Users() {
                                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                                 </svg>
                                 : <>
-                                    <i className="icofont-rocket-alt-1 text-yellow-400 text-4xl"></i>
+                                    <i className="icofont-wallet text-yellow-400 text-4xl"></i>
                                     <p className="text-xs mt-2">VIP1000000004</p>
                                 </>
                             }
@@ -131,7 +135,7 @@ export default function Users() {
                     </button>
 
                 
-                    <button onClick={() => handleMovePage('users/send-e-wallet')} className="btn_menu_user">
+                    <button onClick={() => handleMovePage('users/send-e-wallet')} className="btn_menu_user w-1/4 h-[90px]">
                         <div className=" flex flex-col justify-center items-center">
                             {menuSpinner && itemLink == "users/send-e-wallet" ?
                                 <svg style={{ maxWidth: 40 }} role="status" className="inline w-9 h-9 text-gray-200 animate-spin dark:text-gray-600 fill-red-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -139,13 +143,13 @@ export default function Users() {
                                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                                 </svg>
                                 : <>
-                                    <i className="icofont-rocket-alt-1 text-yellow-400 text-4xl"></i>
-                                    <p className="text-xs mt-2">VIP1000000005</p>
+                                       <i className="icofont-wallet text-4xl  text-green-400"></i>
+                                       <p className="text-xs mt-2">NEW JOIN</p>
                                 </>
                             }
                         </div>
                     </button>
-                    <button onClick={() => handleMovePage('users/send-e-wallet')} className="btn_menu_user">
+                    <button onClick={() => handleMovePage('users/send-e-wallet')} className="btn_menu_user w-1/4 h-[90px]">
                         <div className=" flex flex-col justify-center items-center">
                             {menuSpinner && itemLink == "users/send-e-wallet" ?
                                 <svg style={{ maxWidth: 40 }} role="status" className="inline w-9 h-9 text-gray-200 animate-spin dark:text-gray-600 fill-red-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -171,7 +175,7 @@ export default function Users() {
 
 
                 <div className="w-full flex-between justify-center items-center gap-3 mt-2">
-                    <button onClick={() => handleMovePage('users/confirmation')} className="btn_menu_user">
+                    <button onClick={() => handleMovePage('users/confirmation')} className="btn_menu_user w-1/4 h-[90px]">
                         <div className=" flex flex-col justify-center items-center">
                             {menuSpinner && itemLink == "users/confirmation" ?
                                 <svg style={{ maxWidth: 40 }} role="status" className="inline w-9 h-9 text-gray-200 animate-spin dark:text-gray-600 fill-red-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -179,7 +183,7 @@ export default function Users() {
                                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                                 </svg>
                                 : <>
-                                    <i className="icofont-wallet text-4xl  text-green-400"></i>
+                                    <i className="icofont-rocket-alt-1 text-4xl  text-green-400"></i>
                                     <p className="text-xs mt-2">NEW JOIN</p>
                                 </>
                             }
@@ -189,7 +193,7 @@ export default function Users() {
                  
 
 
-                    <button onClick={() => handleMovePage('users/send-e-wallet')} className="btn_menu_user">
+                    <button onClick={() => handleMovePage('users/send-e-wallet')} className="btn_menu_user w-1/4 h-[90px]">
                         <div className=" flex flex-col justify-center items-center">
                             {menuSpinner && itemLink == "users/send-e-wallet" ?
                                 <svg style={{ maxWidth: 40 }} role="status" className="inline w-9 h-9 text-gray-200 animate-spin dark:text-gray-600 fill-red-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -197,7 +201,7 @@ export default function Users() {
                                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                                 </svg>
                                 : <>
-                                         <i className="icofont-wallet text-4xl  text-green-400"></i>
+                                         <i className="icofont-rocket-alt-1 text-4xl  text-green-400"></i>
                                          <p className="text-xs mt-2">NEW JOIN</p>
                                 </>
                             }
@@ -205,7 +209,7 @@ export default function Users() {
                     </button>
 
                 
-                    <button onClick={() => handleMovePage('users/send-e-wallet')} className="btn_menu_user">
+                    <button onClick={() => handleMovePage('users/send-e-wallet')} className="btn_menu_user w-1/4 h-[90px]">
                         <div className=" flex flex-col justify-center items-center">
                             {menuSpinner && itemLink == "users/send-e-wallet" ?
                                 <svg style={{ maxWidth: 40 }} role="status" className="inline w-9 h-9 text-gray-200 animate-spin dark:text-gray-600 fill-red-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -213,13 +217,13 @@ export default function Users() {
                                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                                 </svg>
                                 : <>
-                                       <i className="icofont-wallet text-4xl  text-green-400"></i>
+                                       <i className="icofont-rocket-alt-1 text-4xl  text-green-400"></i>
                                        <p className="text-xs mt-2">NEW JOIN</p>
                                 </>
                             }
                         </div>
                     </button>
-                    <button onClick={() => handleMovePage('users/send-e-wallet')} className="btn_menu_user">
+                    <button onClick={() => handleMovePage('users/send-e-wallet')} className="btn_menu_user w-1/4 h-[90px]">
                         <div className=" flex flex-col justify-center items-center">
                             {menuSpinner && itemLink == "users/send-e-wallet" ?
                                 <svg style={{ maxWidth: 40 }} role="status" className="inline w-9 h-9 text-gray-200 animate-spin dark:text-gray-600 fill-red-800" viewBox="0 0 100 101" fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -227,7 +231,7 @@ export default function Users() {
                                     <path d="M93.9676 39.0409C96.393 38.4038 97.8624 35.9116 97.0079 33.5539C95.2932 28.8227 92.871 24.3692 89.8167 20.348C85.8452 15.1192 80.8826 10.7238 75.2124 7.41289C69.5422 4.10194 63.2754 1.94025 56.7698 1.05124C51.7666 0.367541 46.6976 0.446843 41.7345 1.27873C39.2613 1.69328 37.813 4.19778 38.4501 6.62326C39.0873 9.04874 41.5694 10.4717 44.0505 10.1071C47.8511 9.54855 51.7191 9.52689 55.5402 10.0491C60.8642 10.7766 65.9928 12.5457 70.6331 15.2552C75.2735 17.9648 79.3347 21.5619 82.5849 25.841C84.9175 28.9121 86.7997 32.2913 88.1811 35.8758C89.083 38.2158 91.5421 39.6781 93.9676 39.0409Z" fill="currentFill" />
                                 </svg>
                                 : <>
-                                        <i className="icofont-wallet text-4xl  text-green-400"></i>
+                                        <i className="icofont-rocket-alt-1 text-4xl  text-green-400"></i>
                                         <p className="text-xs mt-2">NEW JOIN</p>
                                 </>
                             }
@@ -240,8 +244,8 @@ export default function Users() {
                 </div>
 
 
-
-            </div>
+</div>
+          
         </>
     )
 }
